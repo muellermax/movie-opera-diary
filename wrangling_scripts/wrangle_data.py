ccb5e30d216fd57a4df28ecdca9ac030892a2d30
unknown <muellermax1985@gmail.com>
2020-10-25 17:57:08 +0100

    Update: added new plot

--- wrangling_scripts/wrangle_data.py
+++ wrangling_scripts/wrangle_data.py
@@ -106,7 +106,8 @@ def return_figures():
             go.Scatter(
                 x=df.loc[df['title'] == item, 'count'].tolist(),
                 y=df.loc[df['title'] == item, 'evaluation'].tolist(),
-                size=df.loc[df['title'] == item, 'evaluation'].tolist(),
+                mode='markers',
+                marker=dict(size=df.loc[df['title'] == item, 'evaluation'].tolist()),
                 name=item
             )
         )
