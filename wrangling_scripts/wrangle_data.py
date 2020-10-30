c8f2883aa9f735e2c10d5e5984e7fe34ea1ceabb
unknown <muellermax1985@gmail.com>
2020-10-31 00:11:13 +0100

    Update: Working on bubble size

--- wrangling_scripts/wrangle_data.py
+++ wrangling_scripts/wrangle_data.py
@@ -161,7 +161,7 @@ def return_figures():
                 marker=dict(
                     size=df.loc[df['creator'] == item, 'evaluation'].tolist(),
                     sizemode='area',
-                    sizeref=2.*max(df['evaluation'].tolist())/(40.**2)),
+                    sizeref=3,
                 name=item
            )
          )
