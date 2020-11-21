e717939ceab1a1a95e186a8b00bc2fa8aeda3dda
unknown <muellermax1985@gmail.com>
2020-11-21 21:37:15 +0100

    Adding a new site

--- myapp/routes.py
+++ myapp/routes.py
@@ -15,6 +15,22 @@ def index():
     # Convert the plotly figures to JSON for javascript in html template
     figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template('index.html',
+    return render_template('index.html', 
+                           ids=ids,
+                           figuresJSON=figuresJSON)
+
+@app.route('/')
+@app.route('/tmdb')
+def tmdb():
+
+    figures = return_figures()
+
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template('tmdb.html', 
                            ids=ids,
                            figuresJSON=figuresJSON)
