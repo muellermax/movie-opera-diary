788e1face9786c158c60fd9f0d53da6993e8c036
unknown <muellermax1985@gmail.com>
2020-12-27 14:46:49 +0100

    Update: Adding new plot to TMDB

--- wrangling_scripts/wrangle_data_tmdb.py
+++ wrangling_scripts/wrangle_data_tmdb.py
@@ -4,6 +4,37 @@ import plotly.graph_objs as go
 
 df_movie_tmdb = pd.read_csv('https://raw.githubusercontent.com/muellermax/movie-opera-diary/master/wrangling_scripts/input_tmdb.csv')
 
+def me_vs_tmdb_results(df, m):
+    """
+    Function to show the difference of the m items with the highest and lowest 
+    difference in the evaluation. A positive value means that my evaluation is 
+    higher than the tmdb average. 
+    
+    Input: 
+        df (DataFrame): DataFrame with the difference in the evaluations.
+        m (int): How many items should be included (e.g. 'Top-30')
+        
+    Output: 
+        A Seaborn plot. 
+    
+    """
+    
+    df = df.sort_values('diff', ascending = False)
+    
+    # Get the head, tail and middle items
+    head = df.head(m)
+    middle = df.iloc[int(len(df)/2)-int(m/2) : int(len(df)/2) + int(m/2)]
+    tail = df.tail(m)
+
+    # Concat the results
+    all_results = pd.concat([head, middle, tail])
+    
+    # Drop not necessary columns
+    all_results = all_results.drop(['evaluation', 'evaluation_tmdb'], axis = 1)
+
+    return all_results
+
+
 def return_figures_tmdb():
     """
     Creates the plotly visualizations
@@ -16,25 +47,38 @@ def return_figures_tmdb():
 
     """
 
-    # Plots distplot of difference between my evaluation and tmdb    
+    # First plot: Histogram with difference between my evaluation and tmdb    
     df = df_movie_tmdb.copy()
 
     # Select only the diff-column
-    input_var = df['diff']
+    x = df['diff']
 
     graph_one = [go.Histogram(
-        x = input_var)]
+        x = x)]
 
     layout_one = dict(title='Distribution of the difference between TMDBs and my own evaluation',
-                    xaxis=dict(title='Count'),
-                    yaxis=dict(title='Difference (absolute)')
+                    xaxis=dict(title='Difference (absolute)'),
+                    yaxis=dict(title='Count')
                     )
 
-    #print(input_var)
+    # Second plot: Show items with highest positive and negative difference
+    df = me_vs_tmdb_results(df_movie_tmdb, 15)
+
+    graph_two = [go.Bar(
+        x = df['diff'],
+        y = df['title']
+    )]
+
+    layout_two = dict(title='The movies with the highest positive and negative difference as well as the middle section',
+                    xaxis=dict(title='Difference'),
+                    yaxis=dict(title='Title'))
+
+
 
     # append all charts to the figures list
     figures_tmdb = []
     figures_tmdb.append(dict(data=graph_one, layout=layout_one))
+    figures_tmdb.append(dict(data=graph_two, layout=layout_two))
 
     return figures_tmdb
 
