5585f325b5bd050f7bc9a788bcaeae060fe96639
unknown <muellermax1985@gmail.com>
2020-12-27 23:40:36 +0100

    Update: Working on additional tmdb plot

--- wrangling_scripts/wrangle_data_tmdb.py
+++ wrangling_scripts/wrangle_data_tmdb.py
@@ -73,9 +73,9 @@ def return_figures_tmdb():
     layout_two = dict(title='The movies with the highest positive and negative difference as well as the middle section',
                     xaxis=dict(title='Difference'),
                     yaxis=dict(title='Title'),
-                    autosize = False,
+                    autosize = True)
                   #  width = 600,
-                    heigth = 2000)
+                  #  heigth = 2000)
                   #  bargap = 0.75)
 
 
