3ea23f5b69d339bbcfd3e46776eca73ab43c6630
unknown <muellermax1985@gmail.com>
2020-12-29 02:03:33 +0100

    Feature: Working on colorscale in movie diary

--- wrangling_scripts/wrangle_data_movies.py
+++ wrangling_scripts/wrangle_data_movies.py
@@ -90,7 +90,7 @@ def return_figures_movies():
     layout_one = dict(title='Development of movie categories over time',
                       xaxis=dict(title='Date'),
                       yaxis=dict(title='Categories'),
-                      barmode='stack'
+                      barmode='stack',
                       colorscale = dict(
                           sequential = 'Viridis'
                         )
