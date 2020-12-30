db7eae0a58563e43f6352470cf37fbd6ee2bceb7
unknown <muellermax1985@gmail.com>
2020-12-30 23:21:44 +0100

    Update: Added new TMDB plot

--- wrangling_scripts/wrangle_data_tmdb.py
+++ wrangling_scripts/wrangle_data_tmdb.py
@@ -59,11 +59,11 @@ def return_figures_tmdb():
         graph_one.append(go.scatter(
             x = df.loc[df['title'] == item, 'evaluation'],
             y = df.loc[df['title'] == item, 'evaluation_tmdb'],
+         #   name = df.loc[df['title'] == item, 'primary genre'],
             mode = 'markers', 
             marker = dict(
                     size = df.loc[df['title'] == item, 'views'],
-                    sizemode = 'area'),
-            name = df.loc[df['title'] == item, 'primary genre']
+                    sizemode = 'area')
                         )
             )
 
