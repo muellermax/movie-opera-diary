c89f75762b226a3e5e796f3feb214abffb3c7079
unknown <muellermax1985@gmail.com>
2021-01-01 01:50:14 +0100

    Update: Working on bubble size TMDB plot

--- wrangling_scripts/wrangle_data_tmdb.py
+++ wrangling_scripts/wrangle_data_tmdb.py
@@ -68,7 +68,7 @@ def return_figures_tmdb():
             marker = dict(
                     size = df_name.views,
                     sizemode='diameter',
-                    sizeref = 200 #4*max(df_name.views.tolist())/(40.**2)
+                    sizeref = 200, #4*max(df_name.views.tolist())/(40.**2)
                     sizemin = 100)
             )
             )
