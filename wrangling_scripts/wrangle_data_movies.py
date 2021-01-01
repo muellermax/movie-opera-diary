9080ad59980d8415a3ed1445701b3ca1a872f0c7
unknown <muellermax1985@gmail.com>
2021-01-01 20:15:29 +0100

    Update: Working on bubble size movie diary plot

--- wrangling_scripts/wrangle_data_movies.py
+++ wrangling_scripts/wrangle_data_movies.py
@@ -144,7 +144,7 @@ def return_figures_movies():
                 marker=dict(
                     size=df.loc[df['category'] == item, 'evaluation'],
                     sizemode ='diameter',
-                    sizeref = 60, #2.*max(df['evaluation'].tolist())/(40.**2),
+                    sizeref = 3, #2.*max(df['evaluation'].tolist())/(40.**2),
                     sizemin = 7),
                 name=item
             )
